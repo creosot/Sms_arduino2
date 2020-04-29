23b8da3357c3fd9b7cab7486eb7f52b484890709
creosot <creosot@yandex.ru>
2020-04-29 22:07:31 +0300

    prepareSMS

--- VisualGDBCache/Sms_arduino-Debug-Arduino_Mega_or_Mega_2560/context_00_Sms_arduino.h
+++ VisualGDBCache/Sms_arduino-Debug-Arduino_Mega_or_Mega_2560/context_00_Sms_arduino.h
@@ -1,3 +1,3 @@
 #pragma once
-#include "C:/Users/Admin/Source/Repos/creosot/Sms_arduino2/VisualGDBCache/Sms_arduino-Debug-Arduino_Mega_or_Mega_2560/GeneratedPrototypes.h"
+#include "d:/Users/creosot/Source/Repos/creosot/Sms_arduino2/VisualGDBCache/Sms_arduino-Debug-Arduino_Mega_or_Mega_2560/GeneratedPrototypes.h"
 
